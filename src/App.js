e44a3685e768dcb6a526d922a58ae67fc043b731
prmstrgt <pramestiregitaa@gmail.com>
2021-02-05 19:03:02 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -10,20 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('dev'))
 app.use(cors())
 
-const { authUser, authAdmin } = require('../src/middleware/auth')
-const usersRoute = require('../src/routes/user')
-const adminRoute = require('../src/routes/admin')
-const authRoute = require('../src/routes/auth')
-const publicRoute = require('../src/routes/public')
-
-app.use('/user', authUser, usersRoute)
-app.use('/admin', authAdmin, adminRoute)
-app.use('/auth', authRoute)
-app.use('/public', publicRoute)
-
-// provide static files
-app.use('/upload', express.static('assets/uploads/'))
-
 app.listen(APP_PORT, () => {
   console.log(`App listening to port ${APP_PORT}`)
 })
