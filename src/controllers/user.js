dff597e966f310ca468d473f5d6d05a1e513d4f6
prmstrgt <pramestiregitaa@gmail.com>
2021-02-05 20:25:16 +0700

    create CRUD for table Users

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -11,8 +11,43 @@ module.exports = {
       const [user, created] = await User.findOrCreate({ where: { name } })
 
       if (user || created) {
-        return response(res, 'Login successfully', { data: user.name }, 200)
+        return response(res, 'Login successfully', { data: user })
       }
+    } catch (e) {
+      return response(res, e.message, {}, 500, false)
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const { id } = req.params
+      const results = await User.findByPk(id)
+
+      return response(res, `User with id ${id}`, { data: results })
+    } catch (e) {
+      console.log(e)
+      return response(res, e.message, {}, 500, false)
+    }
+  },
+  edit: async (req, res) => {
+    try {
+      const { id } = req.params
+      const { name } = req.body
+      await User.update({ name }, { where: { id } })
+      return response(res, 'Update successfully')
+    } catch (e) {
+      return response(res, e.message, {}, 500, false)
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params
+      const find = await User.findByPk(id)
+      if (!find) {
+        return response(res, 'User not found', {}, 404, false)
+      }
+      await User.destroy({ where: { id } })
+
+      return response(res, 'Delete user successfully')
     } catch (e) {
       return response(res, e.message, {}, 500, false)
     }
--- src/routes/user.js
+++ src/routes/user.js
@@ -2,5 +2,8 @@ const route = require('express').Router()
 const userController = require('../controllers/user')
 
 route.post('/login', userController.create)
+route.get('/get/:id', userController.get)
+route.put('/edit/:id', userController.edit)
+route.delete('/delete/:id', userController.delete)
 
 module.exports = route
